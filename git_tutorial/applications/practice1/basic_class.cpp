1fc3f2d777cbfaa4c43ba9e5ddbe513c03a57d90
quest <quest@ubuntu.myguest.virtualbox.org>
2025-02-21 23:26:11 +0530

    version 1

---
+++ git_tutorial/applications/practice1/basic_class.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+class student
+{
+private:
+    std::string name;
+    int rollno;
+
+public:
+    student(std::string a, int b)
+    {
+        name = a;
+        rollno = b;
+    }
+    void printdetails()
+    {
+        std::cout << name << std::endl;
+        std::cout << rollno << std::endl;
+    }
+};
+int main()
+{
+    student mydet("jenishlin", 25);
+    mydet.printdetails();
+}
